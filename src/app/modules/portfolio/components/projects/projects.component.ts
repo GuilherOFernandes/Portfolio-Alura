a7a43b329c22f81e65f9b7bd3c16f66655a4fe18
GuilherOFernandes <gui_olivernandes@hotmail.com>
2025-04-02 12:53:40 -0300

    Projects feito

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -25,6 +25,48 @@ export class ProjectsComponent {
           href:'https://vidafullstack.com.br',
         }
       ]
-    }
+    },
+    {
+      src: 'assets/img/projects/coloramaa.png',
+      alt:"Colorama",
+      title:"Colorama",
+      width:'140px',
+      height:'51px',
+      description:'<p>Projeto que realizei para a cadeira de Programação Orientada à Objetos</p>',
+      links:[
+        {
+          name:'Conheça o Projeto',
+          href:'https://github.com/GuilherOFernandes/Colorama/tree/main',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/bikcraft.svg',
+      alt:"Bikcraft",
+      title:"Bikcraft",
+      width:'150px',
+      height:'51px',
+      description:'<p>Projeto final do curso de HTML e CSS da Origamid</p>',
+      links:[
+        {
+          name:'Conheça o Projeto',
+          href:'https://guilherofernandes.github.io/bikcraft/',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/animaisfantasticos.png',
+      alt:"Animais Fantasticos",
+      title:"Animais Fantasticos",
+      width:'140px',
+      height:'51px',
+      description:'<p>Projeto principal que fiz sob o curso de JavaScript da Origamid</p>',
+      links:[
+        {
+          name:'Conheça o Projeto',
+          href:'https://guilherofernandes.github.io/AnimaisFantasticos/',
+        }
+      ]
+    },
   ])
 }
